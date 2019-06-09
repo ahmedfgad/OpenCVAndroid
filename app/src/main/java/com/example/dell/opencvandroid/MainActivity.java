ad9e735d0fcec67280150c108d2f80190f843610
Ahmed Gad <ahmed.f.gad@gmail.com>
2019-06-09 21:07:16 +0300

    First commit

---
+++ app/src/main/java/com/example/dell/opencvandroid/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.dell.opencvandroid;
+
+import android.graphics.Bitmap;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        OpenCVLoader.initDebug();
+    }
+
+    public void displayToast(View v){
+
+        Mat img = null;
+
+        try {
+            img = Utils.loadResource(getApplicationContext(), R.drawable.test);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Imgproc.cvtColor(img, img, Imgproc.COLOR_RGB2BGRA);
+
+        Mat img_result = img.clone();
+        Imgproc.Canny(img, img_result, 80, 90);
+        Bitmap img_bitmap = Bitmap.createBitmap(img_result.cols(), img_result.rows(),Bitmap.Config.ARGB_8888);
+        Utils.matToBitmap(img_result, img_bitmap);
+        ImageView imageView = findViewById(R.id.img);
+        imageView.setImageBitmap(img_bitmap);
+    }
+}
